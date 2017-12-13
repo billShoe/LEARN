26026857b405ce728c321589c9aaa35f56306283
Ashleynicole472 <learn@learn16.local>
2017-12-12 16:24:18 -0800

    React Site Prototype

---
+++ React-Example/react-site/src/content.js
@@ -0,0 +1,9 @@
+import React , {Component} from 'react';
+
+export default class Content extends Component {
+    render() {
+        return (
+            <h1 class="light">Biographies are so 2016</h1>
+        )
+    }
+}
