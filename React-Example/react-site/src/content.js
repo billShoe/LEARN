1bc510cb6897a2d1b0f588dfeeb3b27ba1f209af
Justin Pi <shoebill@composte.me>
2017-12-14 20:51:03 +0000

    Basic house keeping

--- React-Example/react-site/src/content.js
+++
@@ -1,9 +0,0 @@
-import React , {Component} from 'react';
-
-export default class Content extends Component {
-    render() {
-        return (
-            <h1 class="light">Biographies are so 2016</h1>
-        )
-    }
-}
